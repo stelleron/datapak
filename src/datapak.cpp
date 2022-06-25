e7f62d59fd0614e04be8ac41bc573421ce7292ca
stelleron <shreyasdonti223@gmail.com>
2022-06-25 12:05:44 +0530

    Reading file header

--- src/datapak.cpp
+++ src/datapak.cpp
@@ -1,5 +1,25 @@
+#include <sys/stat.h>
+#include <iostream>
 #include "snappy.h"
 #include "datapak.hpp"
+
 #define COMP_ARGS(input, output) input.data(), input.size(), &output
 #define COMPRESS(input, output) snappy::Compress(COMP_ARGS(input, output))
 #define DECOMPRESS(input, output) snappy::Uncompress(COMP_ARGS(input, output))
+
+// Impl. for datapak
+Datapak::Datapak(const char* filename) {
+    //== First check if the file exists == 
+    struct stat buffer;
+    bool fileExists = (stat(filename, &buffer) == 0);
+    if (!fileExists) {
+        std::cout << "File does not exist. Creating new .datapak" << std::endl;
+        file.open(filename, std::ios::out); 
+        file.close();
+    }
+    file.open(filename, std::ios::binary);
+    //== Then read the file to get the file header ==
+    // Move read pointer to beginning of the file
+    file.seekg(0, std::ios_base::beg);
+    file.read((char*)&header, sizeof(header));
+}
--- src/datapak.hpp
+++ src/datapak.hpp
@@ -1,7 +1,35 @@
 #ifndef DATAPAK_HPP
     #define DATAPAK_HPP
+    #include <string>
+    #include <fstream>
+    #define DATAPAK_VERSION "0.0.0"
     // Struct for storing the datapak file (extension blind)
     struct Datapak {
+        private:
+            // Struct for the file header
+            struct FileHeader {
+                char version[10];
+                int dataCount;
+            };
+            // Struct for every data header
+            struct DataHeader {
+                char alias[200];
+                int baseSize;
+                int compSize;
+            };
 
+            // Fields
+            std::fstream file;
+            FileHeader header;
+        public:
+            Datapak(const char* filepath); // Load the datapak
+            ~Datapak();
+
+            bool find(const char* alias); // Find whether the given file exists in the datapak
+            int getCompSize(const char* alias); // Get the size of the compressed file in the datapak (in bytes)
+            int getSize(const char* alias); // Get the size of the file decompressed (in bytes)
+
+            void write(const char* alias, const std::string& data); // Compress and write given data to the datapak
+            void read(const char* alias, const std::string* ptr); // Decompress and read data to the given string pointer      
     };
 #endif
