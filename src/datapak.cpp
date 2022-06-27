7f61c9684d74a5c71aaf67c2ce5cdc5d3e5c6b80
stelleron <shreyasdonti223@gmail.com>
2022-06-27 21:06:21 +0530

    Added file initialization

--- src/datapak.cpp
+++ src/datapak.cpp
@@ -10,9 +10,30 @@
 
 // Impl. for datapak
 Datapak::Datapak(const char* filename) {
-
+    //== First check if the file exists == 
+    struct stat buffer;
+    bool fileExists = (stat(filename, &buffer) == 0);
+    if (!fileExists) {
+        std::cout << "File does not exist. Creating new .datapak" << std::endl;
+        file = fopen(filename, "w");
+        fclose(file);
+    }
+    //== Then load the file for read/append operations
+    file = fopen(filename, "a+");
+    //== Then check if the file has been initialised. If not, write a new header to it
+    fseek(file, 0, SEEK_END);
+    {
+        int size = ftell(file);
+        if (size == 0) {
+            std::cout << "File is empty. Initialising file..." << std::endl;
+            FileHeader nHeader;
+                strcpy(nHeader.version, DATAPAK_VERSION);
+                nHeader.dataCount = 0;
+                fwrite(&nHeader, sizeof(nHeader), 1, file);
+        }
+    }
 }
 
 Datapak::~Datapak() {
-    
-}
+    fclose(file);
+}   
